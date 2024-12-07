1179c9fa620f9874ea4f26bdfafa498f0299e0ca
Jeiel Lima Miranda <jeiel@localhost.localdomain>
2024-12-07 09:24:16 -0300

    fix index.js

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ const io = socketio(httpserver);
 
 const gamedirectory = path.join(__dirname, "html");
 
+// Servir arquivos estáticos
 app.use(express.static(gamedirectory));
 
-httpserver.listen(3000);
+// Configuração da porta
+const PORT = process.env.PORT || 3000;
+httpserver.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 var rooms = [];
 var usernames = [];
 
-io.on('connection', function(socket){
-
-  socket.on("join", function(room, username){
-    if (username != ""){
+// Configuração do socket.io
+io.on('connection', function(socket) {
+  socket.on("join", function(room, username) {
+    if (username !== "") {
       rooms[socket.id] = room;
       usernames[socket.id] = username;
       socket.leaveAll();
@@ -27,13 +32,14 @@ io.on('connection', function(socket){
       io.in(room).emit("recieve", "Server : " + username + " has entered the chat.");
       socket.emit("join", room);
     }
-  })
+  });
 
-  socket.on("send", function(message){
-    io.in(rooms[socket.id]).emit("recieve", usernames[socket.id] +" : " + message);
-  })
+  socket.on("send", function(message) {
+    io.in(rooms[socket.id]).emit("recieve", usernames[socket.id] + " : " + message);
+  });
 
-  socket.on("recieve", function(message){
+  socket.on("recieve", function(message) {
     socket.emit("recieve", message);
-  })
-})
+  });
+});
+
